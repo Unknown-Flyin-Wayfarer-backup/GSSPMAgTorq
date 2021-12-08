f1d35fbab6452f2bbe501f0686a2fcd7eca07d44
Unknown-Flyin-Wayfarer-backup <82906990+Unknown-Flyin-Wayfarer-backup@users.noreply.github.com>
2021-12-09 01:11:30 +0530

    Add files via upload

---
+++ Code1.ino
@@ -0,0 +1,15 @@
+int pi=3.14;
+
+void setup() {
+  pinMode(10,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i=0;i<=255;i++){
+    int x=sin(i*pi)/180;
+    int y=map(x,-1,1,0,255);
+    analogWrite(10,y);
+    Serial.println(y);
+    }
+}
